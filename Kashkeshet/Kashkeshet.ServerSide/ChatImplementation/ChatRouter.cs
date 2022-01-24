1bb04ceb528c3a3fe90a8544581767d0695c05d5
Beilinson <adam.v.beili@gmail.com>
2022-01-24 20:19:16 +0200

    began implementing ChatRouter

---
+++ Kashkeshet/Kashkeshet.ServerSide/ChatImplementation/ChatRouter.cs
@@ -0,0 +1,52 @@
+﻿using Kashkeshet.Common.Communicators;
+using Kashkeshet.ServerSide.Core;
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kashkeshet.ServerSide.ChatImplementation
+{
+    public class ChatRouter : ICommunicationRouter
+    {
+        public IRoutableOrganizer RoutableOrganizer { get; }
+        
+        private readonly IFormatter _formatter;
+
+        public ChatRouter(IRoutableOrganizer routableOrganizer, IFormatter formatter)
+        {
+            RoutableOrganizer = routableOrganizer;
+            _formatter = formatter;
+        }
+
+        public void JoinClient(TcpClient client)
+        {
+            var communicator = new TcpCommunicator(client, _formatter);
+            Task.Run(() =>
+            {
+                ProcessCommunications(communicator);
+            });
+        }
+
+        private void ProcessCommunications(ICommunicator communicator)
+        {
+            try
+            {
+                RoutableOrganizer.AddUserToOrganizer(communicator);
+
+                while (true)
+                {
+                    var obj = communicator.Receive();
+
+                    communicator.Send(obj);
+                }
+            }
+            catch
+            {
+                RoutableOrganizer.RemoveUserFromOrganizer(communicator);
+            }
+        }
+    }
+}
--- Kashkeshet/Kashkeshet.ServerSide/Core/IRoutableOrganizer.cs
+++ Kashkeshet/Kashkeshet.ServerSide/Core/IRoutableOrganizer.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Kashkeshet.Common.Communicators;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Kashkeshet.ServerSide.Core
 {
+    // Todo: Might be better suited as DTO with public fields?
     public interface IRoutableOrganizer
     {
+        void AddUserToOrganizer(ICommunicator communicator);
+        void RemoveUserFromOrganizer(ICommunicator communicator);
+        void AddUserToRoute(ICommunicator communicator);
         void CreateRoute(IRoutable routable);
         void EnterRoute(IRoutable routable);
         IEnumerable<IRoutable> GetAvailableRoutes();
